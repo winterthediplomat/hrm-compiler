fe41c1a1a10829648bc4b90dd19e4c03033d35fe
alfateam123 <alfateam123@nwa.xyz>
2017-02-03 23:05:01 +0100

    Add some tests for assignment, alias, add and sub

---
+++ tests/test_parsing_simpleops.py
@@ -0,0 +1,83 @@
+import hrmcompiler.parser as parser
+from io import StringIO
+import pyparsing
+import pytest
+
+
+#######################################################
+
+def test_assign():
+    code = "emp = inbox"
+    with StringIO(code) as f:
+        ast = parser.parse_it(f)
+        
+        assert ast[0].src == "inbox"
+        assert ast[0].dst == "emp"
+
+
+def test_assign_to_inbox():
+    code = "inbox = emp"
+    with StringIO(code) as f:
+        with pytest.raises(ValueError):
+            ast = parser.parse_it(f)
+
+@pytest.mark.skip(reason="we may want this behavior in the future...")
+def test_assign_to_outbox():
+    code = "outbox = emp"
+    with StringIO(code) as f:
+        with pytest.raises(ValueError):
+            ast = parser.parse_it(f)
+
+def test_assign_from_outbox():
+    code = "emp = outbox"
+    with StringIO(code) as f:
+        with pytest.raises(ValueError):
+            ast = parser.parse_it(f)
+
+#######################################################
+
+def test_aliases():
+    code = "alias 5 test"
+    with StringIO(code) as f:
+        ast = parser.parse_it(f)
+
+    assert ast[0].symbolic_name == "test"
+    assert ast[0].tile_no == 5
+
+def test_aliases_tileno_is_not_a_number():
+    code = "alias kek 4"
+    with StringIO(code) as f:
+        with pytest.raises(pyparsing.ParseException):
+            ast = parser.parse_it(f)
+
+#######################################################
+
+def test_add():
+    code = "emp += test"
+    with StringIO(code) as f:
+        ast = parser.parse_it(f)
+
+        assert ast[0].addend == "test"
+
+def test_add_to_nonEmp():
+    """ for now, you cannot add to something different than `emp` """
+    code = "nonemp += test"
+    with StringIO(code) as f:
+        with pytest.raises(pyparsing.ParseException):
+            ast = parser.parse_it(f)
+
+#######################################################
+
+def test_sub():
+    code = "emp -= test"
+    with StringIO(code) as f:
+        ast = parser.parse_it(f)
+
+    assert ast[0].subtraend == "test"
+
+def test_sub_from_nonEmp():
+    """ for now, you cannot sub from something different than `emp` """
+    code = "nonemp -= test"
+    with StringIO(code) as f:
+        with pytest.raises(pyparsing.ParseException):
+            parser.parse_it(f)
