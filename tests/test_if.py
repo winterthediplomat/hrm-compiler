95fa25836f68f652eae064db0674f632a73b1d9e
alfateam123 <alfateam123@nwa.xyz>
2017-02-05 12:30:11 +0100

    implement one-branched if operation
    syntax:
    if <condition> then
      <op1>
      ...
      <opN>
    endif

---
+++ tests/test_if.py
@@ -0,0 +1,41 @@
+import hrmcompiler.parser as parser
+from io import StringIO
+import pyparsing
+import pytest
+
+
+#######################################################
+
+def test_if_single_op():
+    code = """
+    emp = inbox
+    if ez then
+        emp = inbox
+    endif
+    """
+    with StringIO(code) as f:
+        ast = parser.parse_it(f)
+
+    assert type(ast[1]) == parser.IfOp
+    assert ast[1].condition == "ez"
+    assert ast[1].true_branch == [parser.AssignOp("inbox", "emp")]
+
+def test_if_multiple_ops():
+    code = """
+    start:
+    emp = inbox
+    if ez then
+        outbox
+        jmp start
+    endif
+    """
+    with StringIO(code) as f:
+        ast = parser.parse_it(f)
+
+    if_op = ast[2]
+    assert type(if_op) == parser.IfOp
+    assert if_op.condition == "ez"
+    assert if_op.true_branch == [
+            parser.OutboxOp(),
+            parser.JumpOp("start")
+    ]
