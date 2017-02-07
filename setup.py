27b60fc5576101fd3a82acf1b6e73d5b2ecd123f
alfateam123 <alfateam123@nwa.xyz>
2017-02-07 23:41:24 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.0',
+    version='0.1.1',
 
     description='A compiler for a simple language that compiles to Human Resource Machine assembly-like language',
     long_description=long_description,
