b007607e15a7aa890c0223b5125d7a8066cfed28
alfateam123 <alfateam123@nwa.xyz>
2017-02-09 22:22:42 +0100

    implement neg conditional

--- hrmcompiler/parser.py
+++ hrmcompiler/parser.py
@@ -14,7 +14,7 @@ condjump = Group(Keyword("jez") + Word(alphanums))
 program_line = Forward()
 _program_line = (assign | alias | add | sub | outbox | label | jump | condjump | Suppress(pythonStyleComment))
 
-condition = (Keyword("ez") | Keyword("nz"))
+condition = (Keyword("ez") | Keyword("nz") | Keyword("neg"))
 if_block = Group(Suppress(Keyword("if")) + condition + Suppress(Keyword("then"))
            + Group(ZeroOrMore(program_line))
            + Optional(Suppress(Keyword("else")) + Group(ZeroOrMore(program_line)))
